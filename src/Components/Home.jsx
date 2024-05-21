49ae568987a772037e3ac9cc8041e95349ebe09e
Edet John <edetjohn213@gmail.com>
2024-05-20 19:03:18 -0700

    testing: routes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ const Home = () =>{
 
     return(
         <>
-            <h1 className="text-xl font-bold text-red-500">The Meme App is live and running</h1>
+            <h1 className="text-xl font-bold text-red-500">The Meme App is up and running</h1>
         </>
     )
 };
