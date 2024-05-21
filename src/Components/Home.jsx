65183494115e57e98c346f128fcf0f7660d136a5
Edet John <edetjohn213@gmail.com>
2024-05-20 19:17:17 -0700

    testing: routes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Home = () =>{
 
     return(
         <>
-            <h1 className="text-xl font-bold text-red-500">The Meme App is up and running</h1>
+            <h1 className="text-xl font-bold text-red-500">The <Link to="/The-Meme-App/Meme-Feed">Meme</Link> App is up and running</h1>
         </>
     )
 };
